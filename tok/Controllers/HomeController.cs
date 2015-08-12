897aedb7b56f449dfe3e7e4ed26a0825c6d21fd5
iowen <opw@mail.usf.edu>
2015-08-12 05:02:08 -0400

    init

---
+++ tok/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenTokSDK;
+namespace tok.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        static int ApiKey = 45273952; // YOUR API KEY
+        static string ApiSecret = "76fd4d22697620e85e5fdd6bcc3b1f53eed2c7a8";
+        public ActionResult Index()
+        {
+            var OpenTok = new OpenTok(ApiKey, ApiSecret);
+
+            //// Create a session that will attempt to transmit streams directly between clients
+            //var session = OpenTok.CreateSession();
+            //// Store this sessionId in the database for later use:
+            //string sessionId = session.Id;
+
+            // Create a session that uses the OpenTok Media Router (which is required for archiving)
+            var session =  OpenTok.CreateSession(mediaMode: MediaMode.ROUTED);
+            // Store this sessionId in the database for later use:
+           // string sessionId = session.Id;
+
+            //// Create an automatically archived session:
+            //var session = OpenTok.CreateSession(mediaMode: MediaMode.ROUTED, ArchiveMode.ALWAYS);
+            // Store this sessionId in the database for later use:
+            ViewBag.sessionId = session.Id;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ tok/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tok.Startup))]
+namespace tok
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
